1686f1462a964296c876f402f708c523694e8384
acnologiaslayer <support@boomershub.com>
2026-05-04 14:33:14 +0600

    Remove tmux dependency; add arrow-key window navigation
    - vim-test: drop vimux strategy in favor of built-in :terminal (no tmux)
    - vim-options: bind <C-Up/Down/Left/Right> to window navigation alongside <C-h/j/k/l>

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,7 +2,21 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.g.mapleader= " "
 
+vim.g.mapleader = " "
+vim.g.background = "light"
 
+vim.opt.swapfile = false
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<c-Up>',    ':wincmd k<CR>')
+vim.keymap.set('n', '<c-Down>',  ':wincmd j<CR>')
+vim.keymap.set('n', '<c-Left>',  ':wincmd h<CR>')
+vim.keymap.set('n', '<c-Right>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
