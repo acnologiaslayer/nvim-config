fd0dbb4e358c07902e18bb0d22f1beeb67a52eb3
acnologiaslayer <support@boomershub.com>
2026-05-04 14:55:34 +0600

    Add terminal toggle, render-markdown plugin
    - vim-options: <C-`> toggles a bottom-split :terminal in normal & terminal modes
    - plugins/markdown: add render-markdown.nvim for in-buffer markdown rendering

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -20,3 +20,25 @@ vim.keymap.set('n', '<c-Right>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
+
+local function toggle_term()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buftype == 'terminal' then
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == buf then
+          vim.api.nvim_win_close(win, false)
+          return
+        end
+      end
+      vim.cmd('botright split')
+      vim.api.nvim_win_set_buf(0, buf)
+      vim.cmd('startinsert')
+      return
+    end
+  end
+  vim.cmd('botright split | terminal')
+  vim.cmd('startinsert')
+end
+
+vim.keymap.set('n', '<C-`>', toggle_term)
+vim.keymap.set('t', '<C-`>', function() vim.cmd('stopinsert'); toggle_term() end)
